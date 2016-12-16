172ec852fd65eec31008d41d6ecc3149d33466fc
pktrigg <p.kennedy@fugro.com>
2016-12-16 18:05:33 +0800

    removed reference to pyxtf in other folder

--- SonarBeamCorrection.py
+++ SonarBeamCorrection.py
@@ -9,8 +9,6 @@ import bisect
 import math
 import argparse
 import sys
-# good for debugging as we only have the 1 file, but for production we should move these into the same folder as the script so it is all self contained.
-sys.path.append('../pyxtf')
 import csv
 import pyXTF
 import geodetic
